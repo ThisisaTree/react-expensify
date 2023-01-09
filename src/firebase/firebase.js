2d5a3c66ede6cc31dcede68d82c8aa02d9efba7c
Mate <fellner96@gmail.com>
2023-01-09 10:45:03 +0100

    firebase update

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -8,24 +8,50 @@ import { getDatabase, ref, set, update } from "firebase/database";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC7RMvEJy-nJ5FU9zx4QGtAmv-1vll6_9k",
-    authDomain: "expensify-test-a8d36.firebaseapp.com",
-    databaseURL: "https://expensify-test-a8d36-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "expensify-test-a8d36",
-    storageBucket: "expensify-test-a8d36.appspot.com",
-    messagingSenderId: "842929960313",
-    appId: "1:842929960313:web:6393a7c8d4224d10dbaba8",
-    measurementId: "G-VFFHTDJF2W"
+    apiKey: "AIzaSyBzsbGb-U-8Zp-GsEffR9ajDZQ51mnrRYc",
+    authDomain: "expensify-test2-cd9ca.firebaseapp.com",
+    databaseURL: "https://expensify-test2-cd9ca-default-rtdb.firebaseio.com",
+    projectId: "expensify-test2-cd9ca",
+    storageBucket: "expensify-test2-cd9ca.appspot.com",
+    messagingSenderId: "841603260609",
+    appId: "1:841603260609:web:b7ad362a3b89f2da603440",
+    measurementId: "G-XHSM9T0GWX"
   };
   
 
 // Initialize Firebase
 
-const app = initializeApp(config);
+const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getDatabase();
 
 set(ref(db), {
     name: 'Mate',
+    age: 26,
+    isSIngle: false,
+    location: {
+        city: 'Bp',
+        country: 'HU'
+    },
+    animal: 'dog'
   });
   
+const updates = {
+    age: 28
+};
+
+update(ref(db), updates);
+  
+const updates2 = {};
+updates2['/location/city'] = 'Debrecen'
+
+update(ref(db), updates2);
+
+const attributes = {
+    attributes : {
+        height : 190,
+        weight : 97
+    }
+};
+
+update(ref(db), attributes);
