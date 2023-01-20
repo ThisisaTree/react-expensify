9db3e6db60296fdea027c3bfbffb9f3aea14c822
Mate <fellner96@gmail.com>
2023-01-20 10:07:05 +0100

    styling login

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
-import {signInWithGoogle} from '../firebase/firebase';
 
-const LoginPage = () => (
-    <div>
-    <button onClick={signInWithGoogle}>Login</button>
+const LoginPage = (props) => (
+    <div className="box-layout">
+        <div className="box-layout__box">
+            <h1 className="box-layout__title">Expensify App</h1>
+            <p>It is time to get your expenses under control</p>
+            <button onClick={startLogin()}>Login</button>
+        </div>
     </div>
 );
 
-const mapDispatchToProps = (dispatch) => ({
-    startLogin: () => dispatch(startLogin())
-});
+const mapDispatchToProps = (dispatch) => {
+    return {
+        startLogin: () => dispatch(startLogin())
+    };
+};
 
-// export default connect(mapDispatchToProps)(LoginPage);
-export default LoginPage;
+export default connect(mapDispatchToProps)(LoginPage);
+// export default LoginPage;
