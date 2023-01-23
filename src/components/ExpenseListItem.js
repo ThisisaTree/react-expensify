9a6e081cb794173bc3f6c170a74c6fdd160d2c02
Mate <fellner96@gmail.com>
2023-01-23 16:29:57 +0100

    styling expense list and items

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {NavLink } from 'react-router-dom';
+import {Link } from 'react-router-dom';
 import moment from 'moment';
 import numeral from 'numeral';
 
@@ -7,15 +7,15 @@ import numeral from 'numeral';
 const ExpenseListItem = ({id, dispatch, description, amount, createdAt}) => {
 
     return (
+        <Link 
+        className="list-item"
+        to ={`/edit/${id}`}>
         <div>
-        <NavLink to ={`/edit/${id}`}>
-            <h3>{description}</h3>
-        </NavLink>
-        <p>Amount:
-        {numeral(amount).format('$0,0.00')}
-        -
-        {moment(createdAt).format('MMMM Do, YYYY')}</p>
-    </div>
+            <h3 className="list-item__title">{description}</h3>
+            <span className="list-item__sub-title">{moment(createdAt).format('MMMM Do, YYYY')}</span>
+        </div>
+        <h3 className="list-item__data">{numeral(amount).format('$0,0.00')}</h3>
+        </Link>
 )};
 
 export default ExpenseListItem;
