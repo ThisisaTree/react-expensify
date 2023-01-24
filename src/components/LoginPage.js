efc7062ddba6e530576ac4473e271d74bb0fa1f5
Mate <fellner96@gmail.com>
2023-01-24 11:20:30 +0100

    google login removed

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
+import { Link } from 'react-router-dom';
 
-const LoginPage = (props) => (
+const LoginPage = (props) => {
+    
+    return (
     <div className="box-layout">
         <div className="box-layout__box">
             <h1 className="box-layout__title">Expensify</h1>
             <p>It is time to get your expenses under control</p>
-            <button className="button" onClick={startLogin()}>Login with Google</button>
+            <Link className="button"
+                    to="/dashboard">
+                    Login with Google
+            </Link>
         </div>
     </div>
-);
+)};
 
 const mapDispatchToProps = (dispatch) => {
     return {
